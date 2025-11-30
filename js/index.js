6b7c127c79fc79f94a91b5786fcc502a89aca16a
YousefBelal <yousefbelal265@gmail.com>
2025-11-30 12:26:57 +0200

    enhanced: Edit phone logic + added: a specified modal for Add New Phone

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
+let phoneNameEdit = document.querySelector("#phoneNameEdit");
+let phonePriceEdit = document.querySelector("#phonePriceEdit");
+let phoneQtyEdit = document.querySelector("#phoneQtyEdit");
+
+let phoneIndexToEdit = null;
+
 let phones = [
   { name: "iPhone x", price: 400, qty: 3 },
   { name: "iPhone 11", price: 500, qty: 5 },
   { name: "iPhone 12", price: 600, qty: 7 },
 ];
-
-let useEdit = false;
 
 let showPhones = () => {
   let tbody = document.querySelector("table tbody");
@@ -14,35 +18,12 @@ let showPhones = () => {
     tbody.innerHTML += `
     <tr>
       <td>${index + 1}</td>
-      <td>
-        <input
-          id="Name${index + 1}"
-          type="text"
-          disabled="true"
-          value="${el.name}"
-        />
-      </td>
-      <td>
-        <input
-          id="Price${index + 1}"
-          type="number"
-          value="${el.price}"
-          disabled="true"
-        />
-      </td>
-      <td>
-        <input
-          id="Qty${index + 1}"
-          type="number"
-          value="${el.qty}"
-          disabled="true"
-        />
-      </td>
+      <td><input value="${el.name}" disabled/> </td>
+      <td>${el.price} $</td>
+      <td>${el.qty} </td>
       <td>${el.price * el.qty} $</td>
       <td>
-        <button class="btn btn-warning" onclick="editPhone(${
-          index + 1
-        })" id="btn${index + 1}">Edit Phone</button>
+        <button onclick="openModal(${index})" class="btn btn-warning">Edit Phone</button>
         <button onclick="removePhone(${index})" class="btn btn-danger">Remove Phone</button>
       </td>
     </tr> 
@@ -70,44 +51,51 @@ let addNewPhone = () => {
     price: +phonePrice.value,
     qty: +phoneQty.value,
   };
-
-  phones.push(newPhone);
+  let confirmAddNewPhone = confirm("Are you sure?");
+  if (confirmAddNewPhone == true) {
+    phones.push(newPhone);
+  }
   showPhones();
 
   phoneName.value = "";
   phonePrice.value = "";
   phoneQty.value = "";
+  closeAddModal();
 };
 
-let editPhone = (elementIndex) => {
-  let editButton = document.querySelector(`#btn${elementIndex}`);
-  let phoneName = document.querySelector(`#Name${elementIndex}`);
-  let phonePrice = document.querySelector(`#Price${elementIndex}`);
-  let phoneQty = document.querySelector(`#Qty${elementIndex}`);
-  if (useEdit == false) {
-    editButton.innerText = "Done";
-    phoneName.disabled = false;
-    phoneName.style.border = "1px solid #5088ff";
-    phonePrice.disabled = false;
-    phonePrice.style.border = "1px solid #5088ff";
-    phoneQty.disabled = false;
-    phoneQty.style.border = "1px solid #5088ff";
-    useEdit = true;
-  } else {
-    let confirmEdit = confirm("Are you Sure?");
-    if (confirmEdit == true) {
-      phones[elementIndex - 1].name = phoneName.value;
-      phones[elementIndex - 1].price = phonePrice.value;
-      phones[elementIndex - 1].qty = phoneQty.value;
-    }
-    editButton.innerText = "Edit Phone";
-    phoneName.disabled = true;
-    phoneName.style.border = "none";
-    phonePrice.disabled = true;
-    phonePrice.style.border = "none";
-    phoneQty.disabled = true;
-    phoneQty.style.border = "none";
-    useEdit = false;
-    showPhones();
-  }
+let openModal = (indexToEdit) => {
+  phoneIndexToEdit = indexToEdit;
+  let modal = document.querySelector(".myModal");
+  modal.style.display = "flex";
+  let phoneWeWantToEdit = phones[indexToEdit];
+  phoneNameEdit.value = phoneWeWantToEdit.name;
+  phonePriceEdit.value = phoneWeWantToEdit.price;
+  phoneQtyEdit.value = phoneWeWantToEdit.qty;
+};
+
+let closeModal = () => {
+  let modal = document.querySelector(".myModal");
+  modal.style.display = "none";
+};
+
+let updateData = () => {
+  let newPhone = {
+    name: phoneNameEdit.value,
+    price: phonePriceEdit.value,
+    qty: phoneQtyEdit.value,
+  };
+  phones[phoneIndexToEdit] = newPhone;
+  showPhones();
+  closeModal();
+  alert("Phone Edited Successfuly");
+};
+
+let openAddModal = () => {
+  let modal = document.querySelector(".addPhoneModal");
+  modal.style.display = "flex";
+};
+
+let closeAddModal = () => {
+  let modal = document.querySelector(".addPhoneModal");
+  modal.style.display = "none";
 };
